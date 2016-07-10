24b579c1ac8a81b4d3efe8c2b9779eb4101c4fef
Dennis <gdennis91@googlemail.com>
2016-07-10 20:42:29 +0200

    changes

--- head.php
+++ head.php
@@ -8,6 +8,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+  
   <title>SMGR: Slime Mold Graph Repository</title><!-- Bootstrap -->
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <link href="css/sticky_footer.css" rel="stylesheet">
