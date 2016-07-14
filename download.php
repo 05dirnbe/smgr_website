b0c80a989f6e65bce8052f85f36434e8730b9865
Philipp Reichert <prei@me.com>
2016-07-14 22:41:33 +0200

    changes

--- download.php
+++ download.php
@@ -1,5 +1,9 @@
 <?php 
-   header("Content-Type: application/octet-stream");
-   header("Content-Disposition: attachment; filename=".$_GET['path']);
-   readfile($_GET['path']);
+  //header("Content-Type: application/octet-stream");
+  //header("Content-Disposition: attachment; filename=".$_GET['path']);
+  //readfile($_GET['path']);
+    header('Content-Type: application/zip');
+    header('Content-Disposition: attachment; filename=download.zip');
+    header('Pragma: no-cache');
+    readfile("./downloads/download.zip");
 ?>
